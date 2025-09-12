fc23e19f8d79af84b626354e2e8018cb0c060dd9
Rajan Palial <rajanpalial123@gmail.com>
2025-09-12 15:46:07 +0530

    fixed:type_username

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -7,7 +7,7 @@ export const UserSchema = z.object({
 })
 
 export const SignInSchema = z.object({
-  email: z.string().min(3).max(20),
+  name: z.string(),
   password: z.string(),
 })
 export const CreateRoomSchema = z.object({
